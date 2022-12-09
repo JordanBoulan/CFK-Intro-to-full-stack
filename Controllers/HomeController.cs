14c55ca81636e6ac2a235a10c6870b06053d450b
Jordan Boulanger <jordanboulanger1@gmail.com>
2022-12-09 12:23:54 -0800

    add hero page, front page, update readme

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace HelloCWK.Controllers
             return View();
         }
 
+        public IActionResult Hero()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
